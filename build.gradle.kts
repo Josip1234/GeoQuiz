ae4df8a5b02274560b8fe25c4c69d83aeed482e2
Josip Bošnjak <jbosnjak3@gmail.com>
2023-11-19 16:29:40 +0100

    geo quiz is now runnable application

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.2" apply false
+    id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
+val sourceCompatibility by extra("1.8")
+val sourceCompatibility1 by extra(sourceCompatibility)
+val targetCompatibility by extra(1.8)
